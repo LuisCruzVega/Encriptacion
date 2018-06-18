fd7948e0c7072aa6cc4def5ec766887ebc0312ae
LuisCruzVega <39611548+LuisCruzVega@users.noreply.github.com>
2018-06-17 23:35:40 -0500

    Update numero a texto.cpp

--- numero a texto.cpp
+++ numero a texto.cpp
@@ -40,7 +40,7 @@ string convertir(int n){
 	return nombre;
 	}
 	else if (n<30){
-	switch(u){asdfasdfasdf
+	switch(u){
 	case 1: unidad="uno"; break;
 	case 2: unidad="dos"; break;
 	case 3: unidad="tres"; break;
